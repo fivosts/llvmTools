ae7c5b1cf7c99cf410e724b685150b508f429fad
fivosts <fivos_ts@hotmail.com>
2020-02-11 20:57:42 +0000

    Change example to hello

--- Test_Dummy_pass/test_pass.cpp
+++ Test_Dummy_pass/test_pass.cpp
@@ -13,15 +13,16 @@ struct Hello : public FunctionPass {
   }
 
   bool runOnFunction(Function &F) override {
-    errs() << "Hello: ";
-    std::cout << "I am Foivos" << std::endl;
+    errs() << "Hello LLVM";
+    std::cout << "Using stdout" << std::endl;
     errs().write_escaped(F.getName()) << '\n';
+    outs() << "Using outs()\n";
     return false;
   }
 }; // end of struct Hello
 }  // end of anonymous namespace
 
 char Hello::ID = 0;
-static RegisterPass<Hello> X("foivos", "Hello World Pass",
+static RegisterPass<Hello> X("hello", "Hello World Pass",
                              false /* Only looks at CFG */,
                              false /* Analysis Pass */);
